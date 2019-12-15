1063030fe5c1b98f0dac40f05b34282712c66390
Dsafgi-hub <56185620+Dsafgi-hub@users.noreply.github.com>
2019-12-15 23:21:58 +0300

    Добавление в функцию входного параметра

--- task_2.lisp
+++ task_2.lisp
@@ -1,6 +1,10 @@
-(defun readFile ()
-(let ((in (open "C:/Users/dsafg/Downloads/roswell/lab2.txt" :if-does-not-exist nil)))
+(defconstant testFile "C:/Users/dsafg/Downloads/roswell/lab2.txt")
+
+(defun readFile (filepath)
+(let ((in (open filePath :if-does-not-exist nil)))
   (when in
     (loop for line = (read-line in nil)
          while line do (format t "~a~%" line))
     (close in))))
+
+(readfile testfile)
